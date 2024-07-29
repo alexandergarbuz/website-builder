b1a5ef3023e45942e06e6e8f620f051ce2b089b7
alexandergarbuz <alexander.garbuz@gmail.com>
2024-07-28 19:30:33 -0500

    Added FileUtils

---
+++ src/main/java/com/garbuz/web/util/FileUtils.java
@@ -0,0 +1,18 @@
+package com.garbuz.web.util;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+
+import org.apache.commons.io.IOUtils;
+
+public class FileUtils {
+	public static final String readFile(final String path) throws IOException {
+    	try (final InputStream is = FileUtils.class.getResourceAsStream(path)){
+            if (is == null) {
+                throw new IOException("File not found: " + path);
+            }
+            return IOUtils.toString(is, Charset.defaultCharset());
+    	}
+	}
+}
