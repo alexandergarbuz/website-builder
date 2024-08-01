27d1cd85e0c13c7f1e4d71064835010e12e4fe0d
alexandergarbuz <alexander.garbuz@gmail.com>
2024-08-01 08:09:13 -0500

    Added test context

--- src/test/java/com/garbuz/web/WebsiteBuilderApplicationTests.java
+++ src/test/java/com/garbuz/web/WebsiteBuilderApplicationTests.java
@@ -2,8 +2,10 @@ package com.garbuz.web;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class WebsiteBuilderApplicationTests {
 
 	@Test
